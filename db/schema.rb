0ab0a46b75265a05cd2a7c35bd2fee793d9573cb
Denis Petrovsky <vertuss111@gmail.com>
2019-10-26 18:45:34 +0300

    configure mailer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_26_075657) do
+ActiveRecord::Schema.define(version: 2019_10_26_143916) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_10_26_075657) do
     t.datetime "confirmation_sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- test/controllers/billboards_controller_test.rb
+++ test/controllers/billboards_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class BillboardControllerTest < ActionDispatch::IntegrationTest
+class BillboardsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
