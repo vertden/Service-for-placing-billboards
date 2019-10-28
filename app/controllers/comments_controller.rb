a5a7140dec0c080362cdc91b19328abb8e407f0f
Denis Petrovsky <vertuss111@gmail.com>
2019-10-28 11:27:41 +0300

    add comments to billboard page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,31 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  before_action :set_billboard, only: [:new, :create]
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = @billboard.comments.create(comment_params)
+    @comment.nickname =  current_user.nickname
+    respond_to do |format|
+      if @comment.save
+        format.js
+      end
+    end
+  end
+
+
+  private
+
+  def set_billboard
+    @billboard = Billboard.find(params[:billboard_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:nickname,
+                                    :body)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_27_181242) do
+ActiveRecord::Schema.define(version: 2019_10_28_071741) do
 
   create_table "billboard_employments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "body"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2019_10_27_181242) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname"
+    t.text "body"
+    t.integer "billboard_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
