2a85acbe7407d3359898a26ebff4eb6c18e520c4
Denis Petrovsky <vertuss111@gmail.com>
2019-11-04 10:41:21 +0300

    configure action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,17 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
+  # config.action_mailer.delivery_method = :sendmail
+  # config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => Rails.application.credentials.mail_username ,
+      :password             => Rails.application.credentials.mail_password ,
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+  }
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,18 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'tranquil-tor-59072.heroku.com' }
+  config.action_mailer.smtp_settings = {
+      :address              => "smtp.gmail.com",
+      :port                 => 587,
+      :user_name            => ENV['GMAIL_USERNAME'] ,
+      :password             => ENV['GMAIL_PASSWORD'] ,
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+  }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
